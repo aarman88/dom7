778067867877c00a9b5112d1e64b32c0dd4d036b
Арман <@ARMAN>
2023-10-22 23:46:36 +0600

    Добавьте файлы проекта.

---
+++ dom7/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using dom7;
+
+namespace TanksBattle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tank[] t34Tanks = new Tank[5];
+            Tank[] panteraTanks = new Tank[5];
+
+            for (int i = 0; i < 5; i++)
+            {
+                t34Tanks[i] = new Tank("Т-34");
+                panteraTanks[i] = new Tank("Pantera");
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Бой {i + 1}:");
+                Console.WriteLine(t34Tanks[i].GetTankParameters());
+                Console.WriteLine(panteraTanks[i].GetTankParameters());
+
+                if (t34Tanks[i] ^ panteraTanks[i])
+                    Console.WriteLine("Победитель: Т-34");
+                else
+                    Console.WriteLine("Победитель: Pantera");
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ dom7/WorldOfTanks/Tank.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace dom7
+{
+    public class Tank
+    {
+        private string tankName;
+        private int ammunition;
+        private int armor;
+        private int mobility;
+
+        public Tank(string name)
+        {
+            tankName = name;
+            Random random = new Random();
+            ammunition = random.Next(0, 101);
+            armor = random.Next(0, 101);
+            mobility = random.Next(0, 101);
+        }
+
+        public string GetTankParameters()
+        {
+            return $"{tankName}: Ammo={ammunition}, Armor={armor}, Mobility={mobility}";
+        }
+
+        public static bool operator ^(Tank tank1, Tank tank2)
+        {
+            int winCriteria = 0;
+
+            if (tank1.ammunition > tank2.ammunition)
+                winCriteria++;
+            if (tank1.armor > tank2.armor)
+                winCriteria++;
+            if (tank1.mobility > tank2.mobility)
+                winCriteria++;
+
+            return winCriteria >= 2;
+        }
+    }
+}
